0d4e3c38eee2e4d0b24405719c0787294f733248
eriction2 <eriction2@users.noreply.github.com>
2017-04-24 12:16:48 +0200

    Washout included - might be erroneous

--- main.m
+++ main.m
@@ -9,6 +9,7 @@ vx = vx_VBOX;
 SWA = SWA_VBOX;
 vy_mod = vx.*(lr*(lf+lr)*Cf*Cr-lf*Cf*mass.*vx.*vx)./(Ratio*((lf+lr)^2*Cr*Cf+mass.*vx.*vx*(lr*Cr-lf*Cf))).*SWA;
 
+
 %Measurment
 ay_m.time = Time;
 ay_m.signals.values = ay_VBOX+rx.*yawRate_VBOX;
@@ -18,12 +19,18 @@ vx_m.time = Time;
 vx_m.signals.values = vx_VBOX-ry*yawRate_VBOX;
 sim measurments
 
+%Wash-out
+T = 0.7;
+vy_mod_m.time = Time;
+vy_mod_m.signals.values = vy_mod;
+sim washout
 
-
-plot(Time,vy_mod,'r');
+plot(Time,vy_mod,'Color','r','LineWidth',1.5);
 hold on;
-plot(vy_kin.time,vy_kin.Data,'b');
-plot(Time,vy_VBOX+rx.*yawRate_VBOX,'g');
+grid on;
+plot(vy_kin.time,vy_kin.Data,'Color','b','LineWidth',1.5);
+plot(vy_wo.time,vy_wo.Data,'Color','y','LineWidth',1.5);
+plot(Time,vy_VBOX+rx.*yawRate_VBOX,'Color','g','LineWidth',1.5);
 
 
 
